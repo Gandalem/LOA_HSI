33ce9e17bc50ea92c702dcdf0f72542f36779d5b
Gandalem <rbals8429@gmail.com>
2026-06-11 18:48:08 +0900

    feat: render React bracelet official panel

--- frontend/src/components/ResultPanel.jsx
+++ frontend/src/components/ResultPanel.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import BraceletOfficialPanel from './BraceletOfficialPanel.jsx';
 import MarketCostPanel from './MarketCostPanel.jsx';
 import LegacyResultPanel from './ResultPanelLegacy.jsx';
 
 export default function ResultPanel(props) {
-  const marketCost = props.result?.expectedValues?.marketCost;
+  const expectedValues = props.result?.expectedValues || {};
+  const officialBracelet = expectedValues.officialBraceletT4;
+  const marketCost = expectedValues.marketCost;
 
   return (
     <>
       <LegacyResultPanel {...props} />
+      <BraceletOfficialPanel officialBracelet={officialBracelet} />
       <MarketCostPanel marketCost={marketCost} />
     </>
   );
